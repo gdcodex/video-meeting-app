4b3e927b057bc5fae7e0d3862943ce9316bbeda9
Geremsa Daimary <geremsa.daimary@datoms.io>
2022-01-28 16:58:32 +0530

    server setup done

--- backend/server.js
+++ backend/server.js
@@ -2,5 +2,28 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const dotenv = require('dotenv');
+dotenv.config();
+const port = process.env.PORT || 8080;
+//listen server
+server.listen(port,console.log(`Server started on port ${port}`));
 
-server.listen(5000,console.log('Server started on port 5000'));
+app.get('/', (req, res) => {
+    res.send('Video meet app server: ');
+});
+
+io.on('connection', socket => {
+    console.log('user connected');
+    //when user joins a room
+    socket.on('join-room', (roomId, userId, streamId) => {
+        console.log(`${userId} has joined ${roomId}`);
+        socket.join(roomId);q
+        //emit to all users in the room except the user who joined
+        socket.to(roomId).emit('user-connected', userId);
+
+        //on disconnect
+        socket.on('disconnect', () => {
+            socket.to(roomId).emit('user-disconnected', userId, streamId);
+          })
+    })
+})
